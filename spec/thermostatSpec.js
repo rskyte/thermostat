ea213758709c83008cf9b407d8af5365d86e7848
Eleanor Wintram <eleanorwintram@gmail.com>
2017-12-21 14:21:35 +0000

    temp defaults to 20 degrees

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,11 @@
+describe("Thermostat", function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it("defaults to 20 degrees", function() {
+    expect(thermostat.temp).toEqual(20)
+  })
+})
