0fc7abe37e10e942bdb464c344f8f897e2ec7e1a
Eleanor Wintram <eleanorwintram@gmail.com>
2017-12-21 14:30:04 +0000

    adds temp up function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,5 +7,11 @@ describe("Thermostat", function() {
 
   it("defaults to 20 degrees", function() {
     expect(thermostat.temp).toEqual(20)
-  })
+  });
+
+  it("increases the temperature with an up function", function() {
+    thermostat.up(1);
+    expect(thermostat.temp).toEqual(21);
+  });
+
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,3 +3,7 @@ DEFAULT_TEMPERATURE = 20
 function Thermostat() {
   this.temp = DEFAULT_TEMPERATURE;
 };
+
+Thermostat.prototype.up = function(degrees) {
+  this.temp += degrees;
+}
